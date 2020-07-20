55c68a2adf35f0a4bf1698bd8fbc046fa5617673
theboldedpixel <theboldedpixel@gmail.com>
2020-07-19 23:15:49 -0400

    Added more functionality - better interface for showing who's turn it is, randomizing who goes first, lots of CSS transition to go with it, along with rules button. Still need to add ability to play vs CPU

--- app.js
+++ app.js
@@ -32,62 +32,85 @@ const gameFunctions = (() => {
             player1.isTurn = false
             player2.isTurn = true
             gameBoard[e.target.id].clicked = 'x'
-            gameFunctions.checkXWinner(gameBoard)
-            // gameFunctions.checkTie(gameBoard)
+            checkXWinner(gameBoard)
+            changeBackground()
           } else {
             newBox.textContent = 'O'
             player1.isTurn = true
             player2.isTurn = false
             gameBoard[e.target.id].clicked = 'o'
-            gameFunctions.checkOWinner(gameBoard)
-            // gameFunctions.checkTie(gameBoard)
+            checkOWinner(gameBoard)
+            changeBackground()
           }
         }
       })
     })
   }
 
+  const player1Wins = () => {
+    const p1 = document.querySelector('.player1-wins h1')
+    const p2 = document.querySelector('.player1-wins')
+    p1.textContent = `${player1.name} wins the game!`
+    p2.style.display = "block"
+  }
+
+  const player2Wins = () => {
+    const p1 = document.querySelector('.player2-wins h1')
+    const p2 = document.querySelector('.player2-wins')
+    p1.textContent = `${player2.name} wins the game!`
+    p2.style.display = "block"
+  }
+
+  const tieGame = () => {
+    const p1 = document.querySelector('.tie-game h1')
+    const p2 = document.querySelector('.tie-game')
+    p1.textContent = `The game ends in a TIE!`
+    p2.style.display = "block"
+  }
+
   const checkXWinner = (g) => {
+
+
     if (g[0].clicked === 'x' && g[1].clicked === 'x' && g[2].clicked === 'x') {
-      console.log('Player 1 wins')
+      player1Wins()
     } else if ((g[3].clicked === 'x' && g[4].clicked === 'x' && g[5].clicked === 'x')) {
-      console.log('Player 1 wins')
+      player1Wins()
     } else if ((g[6].clicked === 'x' && g[7].clicked === 'x' && g[8].clicked === 'x')) {
-      console.log('Player 1 wins')
+      player1Wins()
     } else if ((g[0].clicked === 'x' && g[3].clicked === 'x' && g[6].clicked === 'x')) {
-      console.log('Player 1 wins')
+      player1Wins()
     } else if ((g[1].clicked === 'x' && g[4].clicked === 'x' && g[7].clicked === 'x')) {
-      console.log('Player 1 wins')
+      player1Wins()
     } else if ((g[2].clicked === 'x' && g[5].clicked === 'x' && g[8].clicked === 'x')) {
-      console.log('Player 1 wins')
+      player1Wins()
     } else if ((g[0].clicked === 'x' && g[4].clicked === 'x' && g[8].clicked === 'x')) {
-      console.log('Player 1 wins')
+      player1Wins()
     } else if ((g[2].clicked === 'x' && g[4].clicked === 'x' && g[6].clicked === 'x')) {
-      console.log('Player 1 wins')
+      player1Wins()
     } else if (checkTie(gameBoard) === undefined) {
-      console.log('Tie game!')
+      tieGame()
     }
   }
 
   const checkOWinner = (g) => {
     if (g[0].clicked === 'o' && g[1].clicked === 'o' && g[2].clicked === 'o') {
-      console.log('Player 2 wins')
+      player2Wins()
     } else if ((g[3].clicked === 'o' && g[4].clicked === 'o' && g[5].clicked === 'o')) {
-      console.log('Player 2 wins')
+      player2Wins()
     } else if ((g[6].clicked === 'o' && g[7].clicked === 'o' && g[8].clicked === 'o')) {
-      console.log('Player 2 wins')
+      player2Wins()
     } else if ((g[0].clicked === 'o' && g[3].clicked === 'o' && g[6].clicked === 'o')) {
-      console.log('Player 2 wins')
+      player2Wins()
     } else if ((g[1].clicked === 'o' && g[4].clicked === 'o' && g[7].clicked === 'o')) {
-      console.log('Player 2 wins')
+      player2Wins()
     } else if ((g[2].clicked === 'o' && g[5].clicked === 'o' && g[8].clicked === 'o')) {
-      console.log('Player 2 wins')
+      player2Wins()
     } else if ((g[0].clicked === 'o' && g[4].clicked === 'o' && g[8].clicked === 'o')) {
-      console.log('Player 2 wins')
+      player2Wins()
     } else if ((g[2].clicked === 'o' && g[4].clicked === 'o' && g[6].clicked === 'o')) {
-      console.log('Player 2 wins')
+      player2Wins()
     } else if (checkTie(gameBoard) === undefined) {
-      console.log('Tie game!')
+      tieGame()
     }
   }
 
@@ -100,33 +123,104 @@ const gameFunctions = (() => {
 
 
   const CreatePlayer1 = () => {
-    const name = prompt('Enter Player 1\'s name:')
-    let isTurn = true
-    return {name, isTurn}
-  }
-
-  const CreatePlayer2 = () => {
-    const name = prompt('Enter Player 2\'s name:')
+    let name = prompt('Enter Player 1\'s name:')
+    !name ? name = 'Player 1' : name = name
     let isTurn = false
     return {name, isTurn}
   }
 
-  return {createGameBoard, checkXWinner, checkOWinner, CreatePlayer1, CreatePlayer2, checkTie}
+  const CreatePlayer2 = () => {
+    let name = prompt('Enter Player 2\'s name:')
+    !name ? name = 'Player 2' : name = name
+    let isTurn = false
+    return {name, isTurn}
+  }
+
+  const newGame = () => {
+    player1.name = ''
+    player2.name = ''
+    player1.isTurn = false
+    player2.isTurn = false
+    gameBoard.forEach(box => box.clicked = 'empty')
+
+    document.querySelector('.game-board').innerHTML = ''
+    document.querySelector('.player1-wins').style.display = 'none'
+    document.querySelector('.player1').style.background = '#cdcdcd'
+    document.querySelector('.player2').style.background = '#cdcdcd'
+    document.querySelector('.player2-wins').style.display = 'none'
+    document.querySelector('.tie-game').style.display = 'none'
+    gameFlow()
+  }
+
+  const changeBackground = () => {
+    const p1Query = document.querySelector('.player1')
+    const p2Query = document.querySelector('.player2')
+    if (!player1.isTurn) {
+      p2Query.style.background = "#fff"
+      p2Query.style.boxShadow = "0 8px 14px rgba(0, 0, 0, .8)"
+      p2Query.style.transform = "translateY(-1px) scale(1.1)"
+      p1Query.style.background = "#cdcdcd"
+      p1Query.style.boxShadow = "0 6px 8px rgba(0, 0, 0, .6)"
+      p1Query.style.transform = "translateY(0) scale(1)"
+    } else {
+      p1Query.style.background = "#fff"
+      p1Query.style.boxShadow = "0 8px 14px rgba(0, 0, 0, .8)"
+      p1Query.style.transform = "translateY(-1px) scale(1.1)"
+      p2Query.style.background = "#cdcdcd"
+      p2Query.style.boxShadow = "0 6px 8px rgba(0, 0, 0, .6)"
+      p2Query.style.transform = "translateY(0) scale(1)"
+    }
+  }
+
+  const whosFirst = () => {
+    const coinFlip = Math.round(Math.random())
+    const p1Query = document.querySelector('.player1')
+    const p2Query = document.querySelector('.player2')
+
+    coinFlip ? player1.isTurn = true : player2.isTurn = true
+
+    if (player1.isTurn) {
+      p1Query.style.background = '#fff'
+      p1Query.style.boxShadow = "0 8px 14px rgba(0, 0, 0, .8)"
+      p1Query.style.transform = "translateY(-1px) scale(1.1)"
+      p2Query.style.background = "#cdcdcd"
+      p2Query.style.boxShadow = "0 6px 8px rgba(0, 0, 0, .6)"
+      p2Query.style.transform = "translateY(0) scale(1)"
+    } else {
+      p2Query.style.background = '#fff'
+      p2Query.style.boxShadow = "0 8px 14px rgba(0, 0, 0, .8)"
+      p2Query.style.transform = "translateY(-1px) scale(1)"
+      p1Query.style.background = "#cdcdcd"
+      p1Query.style.boxShadow = "0 6px 8px rgba(0, 0, 0, .6)"
+      p1Query.style.transform = "translateY(0) scale(1)"
+    }
+    // player1.isTurn ? document.querySelector('.player1').style.background = "#ddd" : document.querySelector('.player2').style.background = "#fff"
+  }
+
+  return {createGameBoard, CreatePlayer1, CreatePlayer2, newGame, whosFirst}
 })()
 
 
 const gameFlow = () => {
   document.querySelector('.play-game').style.display = 'none'
-  document.querySelector('.players p').style.visibility = "visible"
   document.querySelector('.new-game').style.display = "inline-block"
   player1 = gameFunctions.CreatePlayer1()
   player2 =  gameFunctions.CreatePlayer2()
   document.querySelector('.player1').textContent = player1.name
   document.querySelector('.player2').textContent = player2.name
+  gameFunctions.whosFirst()
   gameFunctions.createGameBoard()
 }
 
 document.querySelector('.play-game').addEventListener('click', gameFlow)
+document.querySelectorAll('.new-game').forEach((button) => button.addEventListener('click', gameFunctions.newGame))
+document.querySelector('.rules').addEventListener('click', (e) => {
+  e.target.nextElementSibling.style.display = 'block'
+})
+document.querySelector('.close-rules').addEventListener('click', (e) => {
+  e.target.parentElement.style.display = 'none'
+})
+
 
 
 
